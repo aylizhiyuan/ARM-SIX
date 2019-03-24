4e8893aa91d2606f2b9a6be69d6d0049960ad053
lizhiyuan <148334733@qq.com>
2019-03-24 22:16:43 +0800

    算法效率....

--- 1数据结构的基本概念/1.c
+++ 1数据结构的基本概念/1.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 #include <string.h>
-
+long sum1(int n){
+    long ret = 0;
+    int* array = (int*)malloc(n * sizeof(int));
+    int i = 0;
+    for(i=0;i<n;i++){
+        array[i] = i + 1;
+    }
+    for(i=0;i<n;i++){
+        ret += array[i];
+    }
+    free(array);
+    return ret;
+}
+long sum2(int n){
+    long ret = 0;
+    int i = 0;
+    for(i=1;i<=n;i++){
+        ret += i;
+    }
+    return ret;
+}
+long sum3(int n){
+    long ret = 0;
+    if(n > 0){
+        ret = (1+n)*n/2;
+    }
+    return ret;
+}
 int main(){
-    int a;
-    int b;
+    printf("%ld\n",sum1(100));
+    printf("%ld\n",sum2(100));
+    printf("%ld\n",sum3(100));
+    return 0;
 }
